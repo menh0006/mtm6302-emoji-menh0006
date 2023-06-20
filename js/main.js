7206da8cf79db07f9add9e6d8f1eebff4165b740
Amine Menhich <menh0006@algonquinlive.com>
2023-06-19 22:15:00 -0400

    adds more emojis

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const $emoji = document.getElementById('emoji')
 const emoji= []
 
-for(let i=129410; i<=129422; i++) {
+for(let i=129410; i<=129432; i++) {
     emoji.push (` 
     <div class="emojis">
     <div class="emoji">&#${i}</div>
